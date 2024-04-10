3ace0d56424151a8bdcceb156f88e6ff7f05a138
Pol Vallverdu <86187892+polvallverdu@users.noreply.github.com>
2024-04-10 12:55:17 +0200

    13-weapon

--- src/main/java/me/polvallverdu/cursospigot/PlayerListener.java
+++ src/main/java/me/polvallverdu/cursospigot/PlayerListener.java
@@ -1,17 +1,23 @@
 package me.polvallverdu.cursospigot;
 
-import net.kyori.adventure.text.Component;
-import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.World;
-import org.bukkit.WorldCreator;
+import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 public class PlayerListener implements Listener {
 
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
 
+        if (event.hasItem() && event.getItem().getType() == Material.DIAMOND_HOE) {
+            Arrow arrow = player.launchProjectile(Arrow.class);
+
+            arrow.setDamage(4D);
+        }
+    }
 
 }
--- src/main/java/me/polvallverdu/cursospigot/commands/EjemploCommand.java
+++ src/main/java/me/polvallverdu/cursospigot/commands/EjemploCommand.java
@@ -1,48 +1,24 @@
 package me.polvallverdu.cursospigot.commands;
 
-import net.kyori.adventure.text.Component;
-import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Egg;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
-import org.bukkit.scoreboard.*;
+import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.HashMap;
-import java.util.UUID;
 
 
 public class EjemploCommand implements CommandExecutor {
 
-    private HashMap<UUID, Integer> counter = new HashMap<>();
-
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (sender instanceof Player player) {
-            if (counter.containsKey(player.getUniqueId())) {
-                int newCounter = counter.get(player.getUniqueId())+1;
-                counter.put(player.getUniqueId(), newCounter);
+            ItemStack itemStack = new ItemStack(Material.DIAMOND_HOE);
 
-                player.getScoreboard().getTeam("teamejemplo").suffix(Component.text(newCounter));
-            } else {
-                Scoreboard scoreboard = Bukkit.getScoreboardManager().getNewScoreboard();
-                Objective objective = scoreboard.registerNewObjective("board", Criteria.DUMMY, Component.text("TÍTULO"));
-                objective.setDisplaySlot(DisplaySlot.SIDEBAR);
-
-                Team team = scoreboard.registerNewTeam("teamejemplo");
-                team.addEntry("count");
-                team.prefix(Component.text("Contador: "));
-                team.suffix(Component.text("0"));
-
-                objective.getScore("count").setScore(3);
-                objective.getScore("count").customName(Component.text(""));
-                objective.getScore(" ").setScore(2);
-                objective.getScore("Web: polv.dev").setScore(1);
-                counter.put(player.getUniqueId(), 0);
-
-                player.setScoreboard(scoreboard);
-            }
+            player.getInventory().addItem(itemStack);
         }
 
         return true;
