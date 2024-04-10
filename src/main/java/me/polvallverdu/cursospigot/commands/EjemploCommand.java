84fda15ed147307c6ce8050f3ea0cc5c6b3e2a95
Pol Vallverdu <86187892+polvallverdu@users.noreply.github.com>
2024-04-10 12:09:34 +0200

    10-sound

--- src/main/java/me/polvallverdu/cursospigot/commands/EjemploCommand.java
+++ src/main/java/me/polvallverdu/cursospigot/commands/EjemploCommand.java
@@ -1,34 +1,19 @@
 package me.polvallverdu.cursospigot.commands;
 
-import com.destroystokyo.paper.profile.PlayerProfile;
-import com.destroystokyo.paper.profile.ProfileProperty;
-import org.bukkit.Bukkit;
-import org.bukkit.Color;
-import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.LeatherArmorMeta;
-import org.bukkit.inventory.meta.SkullMeta;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.UUID;
 
 public class EjemploCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        ItemStack armadura = new ItemStack(Material.LEATHER_CHESTPLATE);
-        LeatherArmorMeta meta = (LeatherArmorMeta) armadura.getItemMeta();
-
-        meta.setColor(Color.fromRGB(255, 0, 0));
-
-        armadura.setItemMeta(meta);
-
         if (sender instanceof Player player) {
-            player.getInventory().addItem(armadura);
+            player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 1000f, 0.707107f);
         }
 
         return true;
