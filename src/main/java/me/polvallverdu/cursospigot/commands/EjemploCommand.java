269b6cf16db3e76399859f29d7ca780e6bb3b026
Pol Vallverdu <86187892+polvallverdu@users.noreply.github.com>
2024-04-10 12:19:27 +0200

    11-title

--- src/main/java/me/polvallverdu/cursospigot/commands/EjemploCommand.java
+++ src/main/java/me/polvallverdu/cursospigot/commands/EjemploCommand.java
@@ -1,25 +1,30 @@
 package me.polvallverdu.cursospigot.commands;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.title.Title;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
 import org.jetbrains.annotations.NotNull;
+
+import java.time.Duration;
 
 
 public class EjemploCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        if (sender instanceof Player player) {
-            player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, Integer.MAX_VALUE, 9, true, true, false));
-
-            for (PotionEffect potionEffect : player.getActivePotionEffects()) {
-                player.removePotionEffect(potionEffect.getType());
-            }
-        }
+        sender.showTitle(Title.title(
+                Component.text("Bienvenido!").color(NamedTextColor.AQUA),
+                Component.text("al servidor").color(NamedTextColor.DARK_AQUA),
+                Title.Times.times(
+                        Duration.ofSeconds(1),
+                        Duration.ofSeconds(3),
+                        Duration.ofSeconds(1)
+                )
+        ));
 
         return true;
     }
