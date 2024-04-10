15ad8209e2f392fc352eeb77b0093fb6b619ecd4
Pol Vallverdu <86187892+polvallverdu@users.noreply.github.com>
2024-04-10 12:20:40 +0200

    11-actionbar

--- src/main/java/me/polvallverdu/cursospigot/commands/EjemploCommand.java
+++ src/main/java/me/polvallverdu/cursospigot/commands/EjemploCommand.java
@@ -1,12 +1,10 @@
 package me.polvallverdu.cursospigot.commands;
 
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
-import net.kyori.adventure.title.Title;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 import java.time.Duration;
@@ -16,15 +14,7 @@ public class EjemploCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        sender.showTitle(Title.title(
-                Component.text("Bienvenido!").color(NamedTextColor.AQUA),
-                Component.text("al servidor").color(NamedTextColor.DARK_AQUA),
-                Title.Times.times(
-                        Duration.ofSeconds(1),
-                        Duration.ofSeconds(3),
-                        Duration.ofSeconds(1)
-                )
-        ));
+        sender.sendActionBar(Component.text("Este texto aparece en el actionbar").decorate(TextDecoration.BOLD));
 
         return true;
     }
