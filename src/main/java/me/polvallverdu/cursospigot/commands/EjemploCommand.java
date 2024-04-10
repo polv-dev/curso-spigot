ec55702b93349f56e16e1be2297d626e7316034a
Pol Vallverdu <86187892+polvallverdu@users.noreply.github.com>
2024-04-10 11:50:58 +0200

    mierdon

--- src/main/java/me/polvallverdu/cursospigot/commands/EjemploCommand.java
+++ src/main/java/me/polvallverdu/cursospigot/commands/EjemploCommand.java
@@ -1,39 +1,15 @@
 package me.polvallverdu.cursospigot.commands;
 
-import com.destroystokyo.paper.profile.CraftPlayerProfile;
-import com.destroystokyo.paper.profile.PlayerProfile;
-import com.destroystokyo.paper.profile.ProfileProperty;
-import com.mojang.authlib.GameProfile;
-import net.kyori.adventure.text.Component;
-import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Item;
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.inventory.meta.SkullMeta;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.UUID;
 
 public class EjemploCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        ItemStack cabeza = new ItemStack(Material.PLAYER_HEAD);
 
-        SkullMeta meta = (SkullMeta) cabeza.getItemMeta();
-
-        PlayerProfile playerProfile = new CraftPlayerProfile(UUID.randomUUID(), "hamburguesa");
-        playerProfile.setProperty(new ProfileProperty("textures", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYTZlZjFjMjVmNTE2ZjJlN2Q2Zjc2Njc0MjBlMzNhZGNmM2NkZjkzOGNiMzdmOWE0MWE4YjM1ODY5ZjU2OWIifX19"));
-
-        meta.setPlayerProfile(playerProfile);
-
-        if (sender instanceof Player player) {
-            player.getInventory().addItem(cabeza);
-        }
 
         return true;
     }
