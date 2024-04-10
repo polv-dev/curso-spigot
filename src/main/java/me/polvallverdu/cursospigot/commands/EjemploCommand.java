581e6e6b0d838eef3d40726c075a2c68569eb0a0
Pol Vallverdu <86187892+polvallverdu@users.noreply.github.com>
2024-04-10 12:18:54 +0200

    10-potions

--- src/main/java/me/polvallverdu/cursospigot/commands/EjemploCommand.java
+++ src/main/java/me/polvallverdu/cursospigot/commands/EjemploCommand.java
@@ -1,10 +1,11 @@
 package me.polvallverdu.cursospigot.commands;
 
-import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import org.jetbrains.annotations.NotNull;
 
 
@@ -13,7 +14,11 @@ public class EjemploCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (sender instanceof Player player) {
-            player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 1000f, 0.707107f);
+            player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, Integer.MAX_VALUE, 9, true, true, false));
+
+            for (PotionEffect potionEffect : player.getActivePotionEffects()) {
+                player.removePotionEffect(potionEffect.getType());
+            }
         }
 
         return true;
