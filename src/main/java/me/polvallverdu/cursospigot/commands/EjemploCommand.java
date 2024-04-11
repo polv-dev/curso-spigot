a290ba5d059659aba66ec641dea431b8685fe1af
Pol Vallverdu <86187892+polvallverdu@users.noreply.github.com>
2024-04-11 11:46:30 +0200

    15-particles

--- src/main/java/me/polvallverdu/cursospigot/commands/EjemploCommand.java
+++ src/main/java/me/polvallverdu/cursospigot/commands/EjemploCommand.java
@@ -1,11 +1,14 @@
 package me.polvallverdu.cursospigot.commands;
 
 import me.polvallverdu.cursospigot.CursoSpigot;
-import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Particle;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 
@@ -13,14 +16,16 @@ public class EjemploCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        long ticksTotales = 5*20;
+        if (sender instanceof Player player) {
+            World world = player.getWorld();
 
-        for (int i = 5; i > 0; i--) {
-            int numero = i;
+            for (int i = 0; i < 10; i++) {
+                Location loc = player.getLocation().clone().add(0, i, 0);
 
-            Bukkit.getScheduler().runTaskLater(CursoSpigot.getInstance(), () -> {
-                Bukkit.broadcast(Component.text("Cuenta atras: " + numero));
-            }, ticksTotales - (i*20L));
+                Bukkit.getScheduler().runTaskLater(CursoSpigot.getInstance(), () -> {
+                    world.spawnParticle(Particle.FLAME, loc, 5);
+                }, i*2L);
+            }
         }
 
         return true;
