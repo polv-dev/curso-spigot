2003a9963258d5f72539dd98a84566d1300c5a15
Pol Vallverdu <86187892+polvallverdu@users.noreply.github.com>
2024-04-11 12:28:04 +0200

    17-GUI

--- src/main/java/me/polvallverdu/cursospigot/PlayerListener.java
+++ src/main/java/me/polvallverdu/cursospigot/PlayerListener.java
@@ -1,14 +1,66 @@
 package me.polvallverdu.cursospigot;
 
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 public class PlayerListener implements Listener {
 
+    public static List<UUID> openedMenu = new ArrayList<>();
+
     @EventHandler
     public void onInventoryClick(InventoryClickEvent event) {
+        if (!openedMenu.contains(event.getWhoClicked().getUniqueId())) {
+            return;
+        }
+
         event.setCancelled(true);
+
+        ItemStack item = event.getCurrentItem();
+
+        if (item == null || item.getType() == Material.AIR) {
+            return;
+        }
+
+        Player player = (Player) event.getWhoClicked();
+
+        switch (item.getType()) {
+            case IRON_INGOT -> {
+                player.getInventory().addItem(new ItemStack(Material.IRON_INGOT));
+                player.closeInventory();
+            }
+            case GOLD_INGOT -> {
+                player.getInventory().addItem(new ItemStack(Material.GOLD_INGOT));
+                player.closeInventory();
+            }
+            case DIAMOND -> {
+                player.getInventory().addItem(new ItemStack(Material.DIAMOND));
+                player.closeInventory();
+            }
+            default -> {
+                player.playSound(player, Sound.ENTITY_VILLAGER_NO, 10f, 1f);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onInventoryClose(InventoryCloseEvent event) {
+        openedMenu.remove(event.getPlayer().getUniqueId());
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        openedMenu.remove(event.getPlayer().getUniqueId());
     }
 
 }
--- src/main/java/me/polvallverdu/cursospigot/commands/EjemploCommand.java
+++ src/main/java/me/polvallverdu/cursospigot/commands/EjemploCommand.java
@@ -1,5 +1,9 @@
 package me.polvallverdu.cursospigot.commands;
 
+import me.polvallverdu.cursospigot.PlayerListener;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -7,23 +11,67 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Zombie;
+import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 
 public class EjemploCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        if (sender instanceof Player player) {
-            ItemStack espada = new ItemStack(Material.DIAMOND_SWORD);
-            espada.addEnchantment(Enchantment.DAMAGE_ALL, 3);
+        if (!(sender instanceof Player player)) return true;
 
-            Inventory inventory = player.getInventory();
-            inventory.clear();
-            inventory.addItem(espada);
-        }
+        Inventory inventory = Bukkit.createInventory(null, 54, Component.text("Menú"));
+
+        // Item de hierro
+        ItemStack hierro = new ItemStack(Material.IRON_INGOT);
+        ItemMeta ironMeta = hierro.getItemMeta();
+
+        ironMeta.displayName(Component.text("Hierro").color(NamedTextColor.GRAY));
+        ironMeta.lore(
+                List.of(
+                        Component.text("Esto en un lingote").color(NamedTextColor.RED),
+                        Component.text("de hierro").color(NamedTextColor.RED)
+                )
+        );
+        hierro.setItemMeta(ironMeta);
+
+        // Item de oro
+        ItemStack oro = new ItemStack(Material.GOLD_INGOT);
+        ItemMeta goldMeta = oro.getItemMeta();
+
+        goldMeta.displayName(Component.text("Oro").color(NamedTextColor.GOLD));
+        goldMeta.lore(
+                List.of(
+                        Component.text("Esto en un lingote").color(NamedTextColor.RED),
+                        Component.text("de oro").color(NamedTextColor.RED)
+                )
+        );
+        oro.setItemMeta(goldMeta);
+
+        // Item de diamante
+        ItemStack diamante = new ItemStack(Material.DIAMOND);
+        ItemMeta diamondMeta = diamante.getItemMeta();
+
+        diamondMeta.displayName(Component.text("Diamante").color(NamedTextColor.BLUE));
+        diamondMeta.lore(
+                List.of(
+                        Component.text("Esto en un diamante").color(NamedTextColor.RED)
+                )
+        );
+        diamante.setItemMeta(diamondMeta);
+
+        inventory.setItem(11, hierro);
+        inventory.setItem(13, oro);
+        inventory.setItem(15, diamante);
+
+        PlayerListener.openedMenu.add(player.getUniqueId());
+        player.openInventory(inventory);
 
         return true;
     }
