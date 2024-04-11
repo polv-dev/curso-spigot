6c0bda7f8bada489ae4576fc2a1be8e2c1fdc9f0
Pol Vallverdu <86187892+polvallverdu@users.noreply.github.com>
2024-04-11 11:57:35 +0200

    16-Inventario

--- src/main/java/me/polvallverdu/cursospigot/PlayerListener.java
+++ src/main/java/me/polvallverdu/cursospigot/PlayerListener.java
@@ -1,12 +1,14 @@
 package me.polvallverdu.cursospigot;
 
-import org.bukkit.Material;
-import org.bukkit.entity.Arrow;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
 
 public class PlayerListener implements Listener {
+
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event) {
+        event.setCancelled(true);
+    }
 
 }
--- src/main/java/me/polvallverdu/cursospigot/commands/EjemploCommand.java
+++ src/main/java/me/polvallverdu/cursospigot/commands/EjemploCommand.java
@@ -1,14 +1,14 @@
 package me.polvallverdu.cursospigot.commands;
 
-import me.polvallverdu.cursospigot.CursoSpigot;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.Particle;
-import org.bukkit.World;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Zombie;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
 
@@ -17,15 +17,12 @@ public class EjemploCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (sender instanceof Player player) {
-            World world = player.getWorld();
+            ItemStack espada = new ItemStack(Material.DIAMOND_SWORD);
+            espada.addEnchantment(Enchantment.DAMAGE_ALL, 3);
 
-            for (int i = 0; i < 10; i++) {
-                Location loc = player.getLocation().clone().add(0, i, 0);
-
-                Bukkit.getScheduler().runTaskLater(CursoSpigot.getInstance(), () -> {
-                    world.spawnParticle(Particle.FLAME, loc, 5);
-                }, i*2L);
-            }
+            Inventory inventory = player.getInventory();
+            inventory.clear();
+            inventory.addItem(espada);
         }
 
         return true;
